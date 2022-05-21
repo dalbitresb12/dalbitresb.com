93b84df2670665437b62f3465fdf46028a6cb2d6
Diego Albitres <diego.albitres@gmail.com>
2022-05-21 02:41:40 -0500

    Added site favicons and basic metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,16 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://rsms.me" />
           <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="apple-mobile-web-app-title" content="Diego Albitres" />
+          <meta name="application-name" content="Diego Albitres" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
